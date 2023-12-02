43d4bc3818a960f731b9f9335edc71b667a96a2d
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-12-02 02:15:08 -0800

    actually add attribute file; make reflection slightly less ugly

--- Program.cs
+++ Program.cs
@@ -1,26 +1,34 @@
 ﻿using aoc_solns_2023;
 using System.Reflection;
+using System.Xml.Schema;
 static class Program
 {
     public const string INPUT_FOLDER = @"C:\Users\dninemfive\Documents\workspaces\misc\_aoc\2023\";
     private static void Main()
     {
-        foreach((MethodInfo method, SolutionToProblemAttribute? attr) in Assembly.GetExecutingAssembly()
+        foreach((MethodInfo solution, SolutionToProblemAttribute? attribute) in Assembly.GetExecutingAssembly()
                                              .GetTypes()
                                              .SelectMany(type => type.GetMethods())
                                              .Select(method => (method, attr: method.GetCustomAttribute<SolutionToProblemAttribute>()))
                                              .OrderBy(x => x.attr?.Index))
         {
-            if (attr is null 
-                || !method.IsStatic 
-                || method.ReturnType != typeof(IEnumerable<object>)
-                || method.GetParameters().Select(x => x.ParameterType).First() != typeof(string))
+            if (attribute is null 
+                || !solution.IsStatic 
+                || !solution.ParametersMatch(typeof(IEnumerable<object>), typeof(string)))
                 continue;
-            Console.WriteLine($"Solution for Problem {attr.Index}:");
-            string inputFile = Path.Join(INPUT_FOLDER, $"{attr.Index}_input.txt");
+            Console.WriteLine($"Solution for Problem {attribute.Index}:");
+            string inputFile = Path.Join(INPUT_FOLDER, $"{attribute.Index}_input.txt");
             int partNumber = 1;
-            foreach (object part in (IEnumerable<object>)method.Invoke(null, [inputFile])!)
+            foreach (object part in (IEnumerable<object>)solution.Invoke(null, [inputFile])!)
                 Console.WriteLine($"\tPart {partNumber++}: {part}");
         }
+    }
+    public static bool ParametersMatch(this MethodInfo method, Type returnType, params Type[] argumentTypes)
+    {
+        if(method.ReturnType != returnType) return false;
+        Type[] methodArgTypes = method.GetParameters().Select(x => x.ParameterType).ToArray();
+        return methodArgTypes.Length == argumentTypes.Length 
+            && methodArgTypes.Zip(argumentTypes)
+                             .All(x => x.First == x.Second);
     }
 }
---
+++ SolutionAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc_solns_2023;
+[AttributeUsage(AttributeTargets.Method)]
+public class SolutionToProblemAttribute(int index)
+    : Attribute
+{
+    public int Index { get; private set; } = index;
+}
