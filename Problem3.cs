3ad379251341734767604655c23336729ebce97e
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-12-03 10:54:07 -0800

    problem 3 part 1?

--- Problem3.cs
+++ Problem3.cs
@@ -10,7 +10,7 @@ public static class Problem3
     [SolutionToProblem(3)]
     public static IEnumerable<object> Solve(string[] inputLines)
     {
-        int height = inputLines.Count(), width = inputLines.Select(x => x.Length).Max();
+        int height = inputLines.Length, width = inputLines.Select(x => x.Length).Max();
         char[,] grid = new char[width, height];
         for(int y = 0; y < height; y++)
         {
@@ -19,10 +19,33 @@ public static class Problem3
                 grid[x, y] = inputLines[y][x];
             }
         }
-        yield break;
+        yield return grid.PartNumbers().Sum();
+    }
+    public static IEnumerable<int> PartNumbers(this char[,] plan)
+    {
+        string currentNumber = "";
+        bool isPartNumber = false;
+        foreach((int x, int y) in plan.AllPoints())
+        {
+            char cur = plan[x, y];
+            if(!cur.IsDigit())
+            {
+                if (currentNumber.Length > 0 && isPartNumber)
+                    yield return int.Parse(currentNumber);
+                (currentNumber, isPartNumber) = ("", false);
+                continue;
+            }
+            currentNumber += cur;
+            if ((x, y).IsAdjacentToSymbolIn(plan))
+                isPartNumber = true;
+        } 
     }
     public static bool IsSymbol(this char c) 
-        => c is not (>= '0' and <= '9') and not '.';
+        => !c.IsDigit() && c != '.';
+    public static bool IsDigit(this char c)
+        => c is >= '0' and <= '9';
+    public static bool IsAdjacentToSymbolIn(this (int x, int y) p, char[,] array)
+        => array.ValuesAdjacentTo(p).Any(IsSymbol);
 }
 public readonly struct Point(int x, int y)
 {
@@ -63,4 +86,10 @@ public static class ArrayUtils
     }
     public static IEnumerable<T> ValuesAdjacentTo<T>(this T[,] array, Point point)
         => array.PointsAdjacentTo(point).Select(x => array[point.X, point.Y]);
+    public static IEnumerable<Point> AllPoints<T>(this T[,] array)
+    {
+        for (int x = 0; x < array.GetLength(0); x++)
+            for (int y = 0; y < array.GetLength(1); y++)
+                yield return (x, y);
+    }
 }
