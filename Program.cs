aec62a38161493b675b85c7ddd56828b3dc41393
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-12-03 12:20:57 -0800

    make framework code more readable, at least if you're using intellisense

--- Program.cs
+++ Program.cs
@@ -3,25 +3,41 @@ using System.Reflection;
 static class Program
 {
     public const string INPUT_FOLDER = @"C:\Users\dninemfive\Documents\workspaces\misc\_aoc\2023\";
+    /// <summary>
+    /// Main method for the program. Looks for static methods with the 
+    /// <see cref="SolutionToProblemAttribute">SolutionToProblem</see> attribute with the appropriate
+    /// signature and uses the results to generate output with a consistent structure.
+    /// </summary>
     private static void Main()
     {
-        foreach((MethodInfo solution, SolutionToProblemAttribute? attribute) in Assembly.GetExecutingAssembly()
-                                             .GetTypes()
-                                             .SelectMany(type => type.GetMethods())
-                                             .Select(method => (method, attr: method.GetCustomAttribute<SolutionToProblemAttribute>()))
-                                             .OrderBy(x => x.attr?.Index))
+        // scary query just gets all the loaded methods with the appropriate signature
+        // and associates them with their attribute, if any.
+        foreach((MethodInfo solution, SolutionToProblemAttribute? attribute)
+                in Assembly.GetExecutingAssembly()
+                           .GetTypes()
+                           .SelectMany(type => type.GetMethods())
+                           .Where(HasAppropriateSignature)
+                           .Select(MethodAndAttribute)
+                           .OrderBy(x => x.attr?.Index))
         {
-            if (attribute is null 
-                || !solution.IsStatic 
-                || !solution.ParametersMatch(typeof(IEnumerable<object>), typeof(string[])))
+            if (attribute is null)
                 continue;
             Console.WriteLine($"Solution for Problem {attribute.Index}:");
             string inputFile = Path.Join(INPUT_FOLDER, $"{attribute.Index}_input.txt");
             int partNumber = 1;
-            foreach (object part in (IEnumerable<object>)solution.Invoke(null, [File.ReadAllLines(inputFile)])!)
+            foreach (object part in solution.UsingFile(inputFile))
                 Console.WriteLine($"\tPart {partNumber++}: {part}");
         }
     }
+    /// <summary>
+    /// Checks that the specified method has the desired signature.
+    /// </summary>
+    /// <param name="method">The method whose signature to compare.</param>
+    /// <param name="returnType">The type the method should return.</param>
+    /// <param name="argumentTypes">The type(s), if any, the method should have as arguments</param>
+    /// <returns><see langword="true"/> if the method has the specified 
+    /// <paramref name="argumentTypes"/> and <paramref name="returnType"/>, or
+    /// <see langword="false"/> otherwise.</returns>
     public static bool ParametersMatch(this MethodInfo method, Type returnType, params Type[] argumentTypes)
     {
         if(method.ReturnType != returnType) return false;
@@ -30,4 +46,18 @@ static class Program
             && methodArgTypes.Zip(argumentTypes)
                              .All(x => x.First == x.Second);
     }
+    /// <summary>
+    /// Checks that the given method has the signature desired for AoC 2023 solution methods, i.e. 
+    /// <inheritdoc cref="HasAppropriateSignature(MethodInfo)" path="/sig"/>.
+    /// </summary>
+    /// <sig><c><see langword="static"/> <see cref="IEnumerable{T}">IEnumerable</see>
+    /// &lt;<see langword="string"/>&gt; [method](<see langword="string"/>[])</c></sig>
+    /// <param name="method"></param>
+    /// <returns></returns>
+    public static bool HasAppropriateSignature(this MethodInfo method)
+        => method.IsStatic && method.ParametersMatch(typeof(IEnumerable<object>), typeof(string));
+    public static (MethodInfo method, SolutionToProblemAttribute? attr) MethodAndAttribute(this MethodInfo method)
+        => (method, method.GetCustomAttribute<SolutionToProblemAttribute>());
+    public static IEnumerable<object> UsingFile(this MethodInfo method, string filePath)
+        => (IEnumerable<object>)method.Invoke(null, [File.ReadAllLines(filePath)])!;
 }
--- SolutionAttribute.cs
+++ SolutionAttribute.cs
@@ -1,7 +1,13 @@
 ﻿namespace d9.aoc._23;
+/// <summary>
+/// When applied to a method with a signature of 
+/// <inheritdoc cref="Program.HasAppropriateSignature(System.Reflection.MethodInfo)" path="/sig"/>,
+/// causes <see cref="Program.Main"/> to run it along with other matching methods in order of
+/// <paramref name="index"/>.
+/// </summary>
+/// <param name="index">Which problem the method is a solution for.</param>
 [AttributeUsage(AttributeTargets.Method)]
-public class SolutionToProblemAttribute(int index)
-    : Attribute
+public class SolutionToProblemAttribute(int index) : Attribute
 {
     public int Index { get; private set; } = index;
 }
