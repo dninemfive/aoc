07b72ff26e2935e797d03f2bb49b734a6ce45bb2
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-12-04 01:37:02 -0800

    change problem 3 to use new Grid class instead of extension methods

--- Problem3.cs
+++ Problem3.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace d9.aoc._23;
 public static class Problem3
@@ -10,21 +11,26 @@ public static class Problem3
     [SolutionToProblem(3)]
     public static IEnumerable<object> Solve(string[] inputLines)
     {
-        int height = inputLines.Length, width = inputLines.Select(x => x.Length).Max();
-        char[,] grid = new char[width, height];
-        for(int y = 0; y < height; y++)
-            for(int x = 0; x < width; x++)
-                grid[x, y] = inputLines[y][x];
+        Grid<char> grid = MakeGridFrom(inputLines);
         yield return grid.PartNumbers().Sum();
         yield return grid.GearRatios().Sum();
     }
-    public static IEnumerable<int> PartNumbers(this char[,] plan)
+    public static Grid<char> MakeGridFrom(string[] lines)
+    {
+        int height = lines.Length, width = lines.Select(x => x.Length).Max();
+        char[,] input = new char[width, height];
+        for (int y = 0; y < height; y++)
+            for (int x = 0; x < width; x++)
+                input[x, y] = lines[y][x];
+        return input;
+    }
+    public static IEnumerable<int> PartNumbers(this Grid<char> plan)
     {
         string currentNumber = "";
         bool isPartNumber = false;
-        foreach((int x, int y) in plan.AllPoints())
+        foreach(Point p in plan.AllPoints)
         {
-            char cur = plan[x, y];
+            char cur = plan[p];
             // Console.WriteLine($"({x,3}, {y,3}): {cur} {(isPartNumber ? 'T' : ' ')} {currentNumber}");
             if (!cur.IsDigit())
             {
@@ -34,15 +40,15 @@ public static class Problem3
                 continue;
             }
             currentNumber += cur;
-            if ((x, y).IsAdjacentToSymbolIn(plan))
+            if (p.IsAdjacentToSymbolIn(plan))
                 isPartNumber = true;
         } 
     }
-    public static IEnumerable<int> GearRatios(this char[,] plan)
+    public static IEnumerable<int> GearRatios(this Grid<char> plan)
     {
-        foreach((int x, int y) in plan.GearLocations())
+        foreach(Point p in plan.GearLocations())
         {
-            IEnumerable<int> adjacentNumbers = plan.NumbersAdjacentTo((x, y));
+            IEnumerable<int> adjacentNumbers = plan.NumbersAdjacentTo(p);
             if (adjacentNumbers.Count() != 2)
                 continue;
             yield return adjacentNumbers.Aggregate((x, y) => x * y);
@@ -52,49 +58,47 @@ public static class Problem3
         => !c.IsDigit() && c != '.';
     public static bool IsDigit(this char c)
         => c is >= '0' and <= '9';
-    public static bool IsAdjacentToSymbolIn(this Point p, char[,] array)
-        => array.ValuesAdjacentTo(p, includeSelf: true).Any(IsSymbol);
-    public static bool IsAdjacentToSymbolIn(this (int x, int y) p, char[,] array)
-        => new Point(p).IsAdjacentToSymbolIn(array);
-    public static IEnumerable<int> NumbersAdjacentTo(this char[,] array, Point p)
-    // find adjacent points with digits
-    // for each digit, go left until there stops being digits then go all the way right as normal
-    // if two digits adjacent horizontally take the leftmost one (make sure to account for three adjacent)
+    public static IEnumerable<Point> GearLocations(this Grid<char> grid)
+        => grid.AllPoints.Where(p => grid[p] == '*');
+    public static bool IsAdjacentToSymbolIn(this Point p, Grid<char> grid)
+        => grid.ValuesAdjacentTo(p, includeSelf: true).Any(IsSymbol);
+    public static IEnumerable<int> NumbersAdjacentTo(this Grid<char> grid, Point p)
     {
-        List<Point> digitLocations = array.PointsAdjacentTo(p).Where(p2 => array[p2.X, p2.Y].IsDigit()).ToList();
+        List<Point> digitLocations = grid.PointsAdjacentTo(p)
+                                         .Where(p2 => grid[p2].IsDigit())
+                                         .ToList();
         void removePointsBetween(int left, int right, int y)
             => digitLocations.RemoveAll(p2 => p2.Y == y && p2.X >= left && p2.X <= right);
+
         while(digitLocations.Any())
         {
             Point p2 = digitLocations.First();
-            (int value, int left, int right) = array.EntireNumberIncluding(p2);
+            (int value, int left, int right) = grid.EntireNumberIncluding(p2);
             yield return value;
             removePointsBetween(left, right, p2.Y);
         }        
     }
-    public static (int value, int left, int right) EntireNumberIncluding(this char[,] array, Point p)
+    public static (int value, int left, int right) EntireNumberIncluding(this Grid<char> grid, Point p)
     {
         (int x, int y) = p;
-        char c = array[x, y];
+        char c = grid[p];
         if (!c.IsDigit())
             throw new Exception($"The character {c} at point {p} is not a digit!");
-        int left = x;
-        while (new Point(left - 1, y).IsInBoundsOf(array) && array[left - 1, y].IsDigit())
-            left--;
-        int right = x;
-        while (new Point(right + 1, y).IsInBoundsOf(array) && array[right + 1, y].IsDigit())
-            right++;
+        int findEnd(int step)
+        {
+            int end = x;
+            while (grid.HasInBounds((end + step, y)) && grid[end + step, y].IsDigit())
+                end += step;
+            return end;
+        }
+        int left = findEnd(-1), right = findEnd(1);
         string number = "";
         for(int xi = left; xi <= right; xi++)
         {
-            number += array[xi, y];
+            number += grid[xi, y];
         }
         return (int.Parse(number), left, right);
     }
-    public static IEnumerable<Point> GearLocations(this char[,] array)
-        => array.AllPointsAndValues()
-                .Where(x => x.value == '*')
-                .Select(x => x.point);
 }
 public readonly struct Point(int x, int y)
 {
@@ -115,37 +119,46 @@ public readonly struct Point(int x, int y)
     public override string ToString()
         => $"({X}, {Y})";
 }
-public static class ArrayUtils
+public readonly struct Grid<T>(T[,] grid)
 {
-    public static bool IsInBoundsOf<T>(this Point point, T[,] array)
+    private readonly T[,] _grid = grid;
+    public T this[int x, int y] => _grid[x, y];
+    public T this[Point p] => _grid[p.X, p.Y];
+    public static implicit operator T[,](Grid<T> grid) => grid._grid;
+    public static implicit operator Grid<T>(T[,] grid) => new(grid);
+    public int Width => _grid.GetLength(0);
+    public int Height => _grid.GetLength(1);
+    public bool HasInBounds(Point point)
     {
+
         (int x, int y) = point;
-        return x >= 0 && x < array.GetLength(0) && y >= 0 && y < array.GetLength(1);
+        return x >= 0 && x < Width && y >= 0 && y < Height;
     }
-    public static IEnumerable<Point> PointsAdjacentTo<T>(this T[,] array, Point point, bool includeSelf = false)
+    public IEnumerable<Point> PointsAdjacentTo(Point point, bool includeSelf = false)
     {
         for(int xo = -1; xo <= 1; xo++)
-        {
             for(int yo = -1; yo <= 1; yo++)
             {
                 if (!includeSelf && xo is 0 && yo is 0)
                     continue;
                 Point cur = point + (xo, yo);
-                if (cur.IsInBoundsOf(array))
+                if (HasInBounds(cur))
                     yield return cur;
             }
+    }
+    public IEnumerable<T> ValuesAdjacentTo(Point point, bool includeSelf = false)
+    {
+        // "lambda methods (&c) don't have access to `this`" for some reason
+        Grid<T> _this = this;
+        return PointsAdjacentTo(point, includeSelf).Select(x => _this[x]);
+    }
+    public IEnumerable<Point> AllPoints
+    {
+        get
+        {
+            for (int y = 0; y < Height; y++)
+                for (int x = 0; x < Width; x++)
+                    yield return (x, y);
         }
     }
-    public static IEnumerable<T> ValuesAdjacentTo<T>(this T[,] array, Point point, bool includeSelf = false)
-        => array.PointsAdjacentTo(point, includeSelf).Select(p => array[p.X, p.Y]);
-    public static IEnumerable<(Point point, T value)> PointsAndValuesAdjacentTo<T>(this T[,] array, Point point, bool includeSelf = false)
-        => array.PointsAdjacentTo(point, includeSelf).Select(p => (p, array[p.X, p.Y]));
-    public static IEnumerable<Point> AllPoints<T>(this T[,] array)
-    {
-        for (int y = 0; y < array.GetLength(1); y++)
-            for (int x = 0; x < array.GetLength(0); x++)
-                yield return (x, y);
-    }
-    public static IEnumerable<(Point point, T value)> AllPointsAndValues<T>(this T[,] array)
-        => array.AllPoints().Select(p => (p, array[p.X, p.Y]));
 }
