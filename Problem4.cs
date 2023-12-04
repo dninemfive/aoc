f6f00ec6c6f75f1edb3d4fd891894d55e3b27f44
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-12-04 12:04:34 -0800

    4.1 solution

---
+++ Problem4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d9.aoc._23;
+public static class Problem4
+{
+    [SolutionToProblem(4)]
+    public static IEnumerable<object> Solve(string[] inputLines)
+    {
+        IEnumerable<ScratchCard> scratchCards = inputLines.Select(x => new ScratchCard(x));
+        yield return scratchCards.Select(x => x.Value).Sum();
+    }
+}
+public class ScratchCard
+{
+    public int CardNumber { get; private set; }
+    public IEnumerable<int> WinningNumbers { get; private set; }
+    public IEnumerable<int> HasNumbers { get; private set; }
+    public ScratchCard(string line)
+    {
+        List<string> split = line.SplitAndTrim(": ", " | ");
+        CardNumber = int.Parse(split[0].SplitAndTrim(" ")[1]);
+        WinningNumbers = split[1].ToInts();
+        HasNumbers = split[2].ToInts();
+    }
+    public int WinningNumberCount 
+        => HasNumbers.Distinct()
+                     .Where(x => WinningNumbers.Contains(x))
+                     .Count();
+    public int Value
+        => WinningNumberCount > 0 ? (int)Math.Pow(2, WinningNumberCount - 1) : 0;
+}
--- Utils.cs
+++ Utils.cs
@@ -10,4 +10,18 @@ public static class Utils
 {
     public static string ListNotation<T>(this IEnumerable<T> enumerable)
         => enumerable.Any() ? enumerable.Select(x => $"{x}").Aggregate((x, y) => $"{x}, {y}") : "(no elements)";
+    public static IEnumerable<int> ToInts(this string s, string delimiter = " ")
+        => s.SplitAndTrim(delimiter).Select(x => int.Parse(x.Trim()));
+    public static List<string> SplitAndTrim(this string s, params string[] delimiters)
+    {
+        List<string> result = [s];
+        foreach(string delimiter in delimiters)
+        {
+            List<string> newResult = new();
+            foreach (string s2 in result)
+                newResult.AddRange(s2.Split(delimiter, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries));
+            result = newResult;
+        }
+        return result;
+    }
 }
