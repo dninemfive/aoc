e6e0cb95d0c7991790f4017cd2c3f421cf98d061
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-12-05 15:36:14 -0800

    ...in progress...

--- Problem5.cs
+++ Problem5.cs
@@ -7,13 +7,47 @@ using System.Threading.Tasks;
 namespace d9.aoc._23;
 public class Problem5
 {
-
+    [SolutionToProblem(5)]
+    public IEnumerable<object> Solve(int[] lines)
+    {
+        
+        yield break;
+    }    
+    // possible paths:
+    // seed -> soil -> fertilizer -> water -> light -> temperature -> humidity -> location
+    // ...oh. that's the only one.
+    public int LocationFor(int seed, Dictionary<string, XToYMap> mapMap)
+    {
+        (string type, int val) cur = ("seed", seed);
+        while(curType != "location")
+        {
+            XToYMap map = mapMap[cur.type];
+            cur = (map.To, map[cur.val]);
+        }
+    }
 }
-public class XToYMap(string title, string lines)
+public class XToYMap(string title, IEnumerable<string> nonTitleLines)
 {
     public string From => title.Split("-")[0];
     public string To => title.Split("-")[2];
-
+    public string Name => $"{From}-to-{To} map";
+    private readonly List<MapRange> _ranges = [ .. nonTitleLines.Select(x => new MapRange(x))
+                                                                .OrderBy(x => x.Source.Start) ];
+    public int this[string sourceType, string destType, int n]
+    {
+        get
+        {
+            if (sourceType != From || destType != To)
+                throw new ArgumentException($"A {Name} can't convert from {sourceType} to {destType}!");
+            foreach (MapRange range in _ranges)
+            {
+                int? result = range[n];
+                if (result is not null)
+                    return result.Value;
+            }
+            return n;
+        }
+    }
 }
 public readonly struct Range(int start, int length)
 {
