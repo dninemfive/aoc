12455e0902a2a6127e5fffde99c5dd91b0fa51b8
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-12-08 12:05:57 -0800

    minor problem 7 code cleanup

--- Problem7.cs
+++ Problem7.cs
@@ -10,18 +10,22 @@ public static class Problem7
     [SolutionToProblem(7)]
     public static IEnumerable<object> Solve(string[] lines)
     {
-        IEnumerable<Hand> hands = lines.Select(x => new Hand(x)).OrderDescending();
-        IEnumerable<int> ranks = hands.Rank();
-        yield return hands.Zip(ranks).Select(x => x.First.Bet * x.Second).Sum();
-        hands = lines.Select(x => new Hand(x, jokerMode: true)).OrderDescending();
-        ranks = hands.Rank();
-        yield return hands.Zip(ranks).Select(x => x.First.Bet * x.Second).Sum();
+        yield return Winnings(lines, jokerMode: false);
+        yield return Winnings(lines, jokerMode: true);
     }
     public static IEnumerable<int> Rank(this IEnumerable<Hand> hands)
     {
         int rank = hands.Count();
         foreach (Hand hand in hands.OrderDescending())
             yield return rank--;
+    }
+    public static int Winnings(IEnumerable<string> lines, bool jokerMode)
+    {
+        IEnumerable<Hand> hands = lines.Select(x => new Hand(x, jokerMode)).OrderDescending();
+        IEnumerable<int> ranks = hands.Rank();
+        return hands.Zip(ranks)
+                    .Select(x => x.First.Bet * x.Second)
+                    .Sum();
     }
 }
 public readonly struct Hand
@@ -36,26 +40,12 @@ public readonly struct Hand
         Cards = split.First().Select(x => new CamelCard(x, jokerMode)).ToList();
         Bet = split.Second().Parse<int>();
         JokerMode = jokerMode;
-    }
-    public string Name => Cards.Select(x => x.Name).Merge();
-    public override string ToString() => $"{Type,-12} {Name} {Runs.ListNotation()}";
-    public int CompareTo(Hand other)
-    {
-        if(other.Type != Type)
-        {
-            // i accidentally ordered the enum backward lol
-            return other.Type.CompareTo(Type);
-        }
-        for(int i = 0; i < Cards.Count(); i++)
-        {
-            if (this[i] == other[i])
-                continue;
-            return this[i].CompareTo(other[i]);
-        }
-        return 0;
-    }
+    }    
     public IEnumerable<CamelCard> UniqueCards => Cards.Distinct();
-    public Run Run(CamelCard cc)
+    public IEnumerable<CamelCard> NonJokerCards => UniqueCards.Where(x => x != 'J');
+    public int Count(CamelCard cardType)
+        => Cards.Count(x => x == cardType);
+    public Run RunFor(CamelCard cc)
         => (cc, Count(cc));
     public IEnumerable<Run> Runs
     {
@@ -63,23 +53,22 @@ public readonly struct Hand
         {            
             if(JokerMode && UniqueCards.Any(x => x == 'J'))
             {
-                List<Run> result = [.. UniqueCards.Where(x => x != 'J')
-                                                  .Select(Run)
-                                                  .OrderByDescending(x => x.Count)];
-                (_, int jokerCount) = Run(new('J', true));
+                List<Run> result = [.. NonJokerCards.Select(RunFor)
+                                                    .OrderByDescending(x => x.Count)];
+                (_, int jokerCount) = RunFor(new('J', true));
                 if (!result.Any())
-                    return [Run(new('J', true))];
+                    return [RunFor(new('J', true))];
                 result[0] = result.First() + jokerCount;
                 return result;
             } 
             else
             {
-                return UniqueCards.Select(Run).OrderByDescending(x => x.Count);
+                return UniqueCards.Select(RunFor).OrderByDescending(x => x.Count);
             }
         }
     }
-    public int Count(CamelCard cardType)
-        => Cards.Count(x => x == cardType);
+    // this particular implementation stolen from OtherwiseJunk, because i accidentally spoiled myself.
+    // it was quite similar to what i already had, though :willemsmug:
     public HandType Type => Runs.Select(x => x.Count).ToList() switch
     {
         [5] => HandType.FiveOfAKind,
@@ -90,7 +79,22 @@ public readonly struct Hand
         [2, 1, 1, 1] => HandType.OnePair,
         _ => HandType.HighCard
     };
-    public CamelCard this[int index] => Cards.ElementAt(index);
+    public CamelCard this[int index] => Cards.ElementAt(index); 
+    public int CompareTo(Hand other)
+    {
+        if (other.Type != Type)
+        {
+            // i accidentally ordered the enum backward lol
+            return other.Type.CompareTo(Type);
+        }
+        for (int i = 0; i < Cards.Count(); i++)
+        {
+            if (this[i] == other[i])
+                continue;
+            return this[i].CompareTo(other[i]);
+        }
+        return 0;
+    }
 }
 public enum HandType
 {
@@ -105,7 +109,6 @@ public enum HandType
 public readonly struct CamelCard(char c, bool jokerMode = false) : IEquatable<CamelCard>, IComparable<CamelCard>
 {
     private readonly char _c = c;
-    public string Name => $"{_c}";
     public readonly bool JokerMode = jokerMode;
     public int Value => _c switch
     {
@@ -117,9 +120,8 @@ public readonly struct CamelCard(char c, bool jokerMode = false) : IEquatable<Ca
         >= '0' and <= '9' => _c - '0',
         _ => throw new ArgumentOutOfRangeException(nameof(_c))
     };
-    public static implicit operator string(CamelCard cc) => cc.Name;
+    #region miscellaneous operators
     public static implicit operator char(CamelCard cc) => cc._c;
-    public override string ToString() => $"{Name}{(JokerMode ? "'" : "")}";
     public override bool Equals(object? obj) 
         => obj is CamelCard card && Equals(card);
     public bool Equals(CamelCard other)
@@ -132,7 +134,9 @@ public readonly struct CamelCard(char c, bool jokerMode = false) : IEquatable<Ca
         => a.JokerMode == b.JokerMode && a.Value == b.Value;
     public static bool operator !=(CamelCard a, CamelCard b) 
         => !(a == b);
+    #endregion miscellaneous operators
 }
+// basically just a tuple, but "Run" is a lot shorter than "(CamelCard card, int count)"
 public readonly struct Run(CamelCard card, int ct)
 {
     public readonly CamelCard Card = card;
@@ -146,6 +150,4 @@ public readonly struct Run(CamelCard card, int ct)
         => new(tuple.card, tuple.ct);
     public static Run operator +(Run run, int amt) 
         => (run.Card, run.Count + amt);
-    public override string ToString()
-        => $"({Card}, {Count})";
 }
