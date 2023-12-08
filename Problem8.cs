8463549cfa046e05160ddfa4da1bef4427c916b4
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-12-08 02:39:47 -0800

    am i stupid??

--- Problem8.cs
+++ Problem8.cs
@@ -19,7 +19,7 @@ public static class Problem8
             _nodes[vals.First()] = (vals[1], vals[2]);
         }
         yield return NavigateBetween("AAA", "ZZZ", tape);
-        yield return GhostNavigate();
+        yield return GhostNavigate(tape);
     }
     public static int NavigateBetween(string start, string end, string tape)
         => start.NavigateUntil(x => x == end, tape);
@@ -40,51 +40,63 @@ public static class Problem8
         char c = tape.Advance();
         return c == 'L' ? left : right;
     }
-    public static int GhostNavigate()
+    public static int GhostNavigate(string tape)
     {
-        // todo: find where the sequence repeats (i.e. encounters a (node, direction, index) threeple twice)
-        // but recently the sequence has repeated and repeated and it leaves me with the theory that they're trying to get inside
-        _tape.Reset();
-        List<string> starts = _nodes.Keys.Where(x => x.EndsWith('A')).ToList();
-        return starts.Select(x => x.NavigateUntil(x => x.EndsWith('Z'))).LeastCommonFactor();
+        return _nodes.Keys.Where(x => x.EndsWith('A'))
+                          .SelectMany(tape.ZPositions)
+                          .LeastCommonFactor();
     }
     public static IEnumerable<int> Factors(this int n)
     {
+        Console.WriteLine($"Factors({n})");
         // making sure to perform each calculation once in a thickheaded way to improve performance
         int halfN = n / 2;
-        for(float f = 0; f < halfN; f++)
+        for(float f = 1; f < halfN; f++)
         {
             float factor = n / f;
             int intFactor = (int)factor;
             if (factor == intFactor)
+            {
+                Console.WriteLine($"\t{factor}");
                 yield return intFactor;
+            }
         }
     }
     public static int LeastCommonFactor(this IEnumerable<int> ints)
     {
+        Console.WriteLine($"{nameof(LeastCommonFactor)}({ints.ListNotation()})");
         // todo: we can eliminate low factors once any item doesn't include them,
         // so add an argument "min" to factors
-        IEnumerable<IEnumerable<int>> factorLists = ints.Select(x => x.Factors());
+        IEnumerable<IEnumerable<int>> factorLists = ints.Select(Factors);
         foreach (int factor in factorLists.SelectMany(x => x).Order())
             if (factorLists.All(x => x.Contains(factor)))
                 return factor;
         return ints.Distinct().Aggregate((x, y) => x * y);
     }
-    public static IEnumerable<int> ZIndexes(string cur, string input)
+    public static IEnumerable<int> ZPositions(this string input, string cur)
     {
+        Console.WriteLine($"{nameof(ZPositions)}({cur})");
         Tape tape = new(input);
         HashSet<(string cur, int index)> visitedStates = new();
+        int ct = 0;
+        // continue until the sequence starts repeating and repeating and leaving you with the theory that they're trying to get inside
         while(!visitedStates.Contains((cur, tape.Index)))
         {
             if (cur.EndsWith('Z'))
-                yield return tape.Index;
+            {
+                Console.WriteLine($"\t{cur} {ct}");
+                yield return ct;
+            }
+            visitedStates.Add((cur, tape.Index));
             cur = tape.Step(cur);
+            ct++;
         }
     }
 }
 public class Tape(string s)
 {
-    public readonly string Items = s;
+    public readonly string Items = s.All(x => x is 'L' or 'R') ? s 
+        : throw new ArgumentException("String for a Tape must consist only of characters L and R!");
     private int _index = 0;
     public int Index
     {
