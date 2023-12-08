331e6ff3872a6f24836a9911d88fccba9d3909bb
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-12-08 02:20:52 -0800

    stuff

--- Problem8.cs
+++ Problem8.cs
@@ -9,38 +9,35 @@ namespace d9.aoc._23;
 public static class Problem8
 {
     private static readonly Dictionary<string, (string left, string right)> _nodes = new();
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value... i know it will have a non-null value when needed
-    private static Tape _tape;
-#pragma warning restore CS8618
     [SolutionToProblem(8)]
     public static IEnumerable<object> Solve(string[] lines)
     {
-        _tape = new(lines.First());
+        string tape = lines.First();
         foreach(string s in lines[2..])
         {
             List<string> vals = s.SplitAndTrim(" = (", ", ", ")");
             _nodes[vals.First()] = (vals[1], vals[2]);
         }
-        yield return NavigateBetween("AAA", "ZZZ");
+        yield return NavigateBetween("AAA", "ZZZ", tape);
         yield return GhostNavigate();
     }
-    public static int NavigateBetween(string start, string end)
-        => start.NavigateUntil(x => x == end);
-    public static int NavigateUntil(this string start, Func<string, bool> end) {
-        _tape.Reset();
+    public static int NavigateBetween(string start, string end, string tape)
+        => start.NavigateUntil(x => x == end, tape);
+    public static int NavigateUntil(this string start, Func<string, bool> end, string input) {
+        Tape tape = new(input);
         string cur = start;
         int ct = 0;
         while (!end(cur))
         {
-            cur = Step(cur, advance: true);
+            cur = tape.Step(cur);
             ct++;
         }
         return ct;
     }
-    public static string Step(string cur, bool advance = false)
+    public static string Step(this Tape tape, string cur)
     {
         (string left, string right) = _nodes[cur];
-        char c = advance ? _tape.Advance() : _tape.Current;
+        char c = tape.Advance();
         return c == 'L' ? left : right;
     }
     public static int GhostNavigate()
@@ -72,6 +69,17 @@ public static class Problem8
             if (factorLists.All(x => x.Contains(factor)))
                 return factor;
         return ints.Distinct().Aggregate((x, y) => x * y);
+    }
+    public static IEnumerable<int> ZIndexes(string cur, string input)
+    {
+        Tape tape = new(input);
+        HashSet<(string cur, int index)> visitedStates = new();
+        while(!visitedStates.Contains((cur, tape.Index)))
+        {
+            if (cur.EndsWith('Z'))
+                yield return tape.Index;
+            cur = tape.Step(cur);
+        }
     }
 }
 public class Tape(string s)
