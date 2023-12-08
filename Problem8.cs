a66951d01a72c52cdbd66db1f22310091ec99545
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-12-08 01:58:22 -0800

    brute forcing is probably too slow lol

--- Problem8.cs
+++ Problem8.cs
@@ -8,30 +8,53 @@ namespace d9.aoc._23;
 public static class Problem8
 {
     private static readonly Dictionary<string, (string left, string right)> _nodes = new();
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value... i know it will have a non-null value when needed
+    private static Tape _tape;
+#pragma warning restore CS8618
     [SolutionToProblem(8)]
     public static IEnumerable<object> Solve(string[] lines)
     {
-        Tape tape = new(lines.First());
+        _tape = new(lines.First());
         foreach(string s in lines[2..])
         {
             List<string> vals = s.SplitAndTrim(" = (", ", ", ")");
             _nodes[vals.First()] = (vals[1], vals[2]);
-            Console.WriteLine($"{vals.First()} = ({vals[1]}, {vals[2]})");
         }
-        yield return NavigateBetween("AAA", "ZZZ", tape);
+        yield return NavigateBetween("AAA", "ZZZ");
+        yield return GhostNavigate();
     }
-    public static int NavigateBetween(string start, string end, Tape tape)
+    public static int NavigateBetween(string start, string end)
     {
+        _tape.Reset();
         string cur = start;
         int ct = 0;
         while (cur != end)
         {
-            (string left, string right) = _nodes[cur];
-            Console.WriteLine($"{ct,4} {cur} {tape.Index,3} {tape.Current} {left} {right}");
-            cur = tape.Advance() == 'L' ? left : right;
+            cur = Step(cur, advance: true);
             ct++;
         }
-        Console.WriteLine($"{ct,4} {cur}");
+        return ct;
+    }
+    public static string Step(string cur, bool advance = false)
+    {
+        (string left, string right) = _nodes[cur];
+        char c = advance ? _tape.Advance() : _tape.Current;
+        return c == 'L' ? left : right;
+    }
+    public static int GhostNavigate()
+    {
+        _tape.Reset();
+        List<string> curs = _nodes.Keys.Where(x => x.EndsWith('A')).ToList();
+        Console.WriteLine(curs.ListNotation());
+        int ct = 0;
+        while(curs.Any(x => !x.EndsWith('Z')))
+        {
+            for (int i = 0; i < curs.Count; i++)
+                curs[i] = Step(curs[i]);
+            _ = _tape.Advance();
+            Console.WriteLine($"{ct,6} {curs.Select(x => x.EndsWith('Z') ? "Z" : " ").Merge()}");
+            ct++;
+        }
         return ct;
     }
 }
@@ -56,4 +79,6 @@ public class Tape(string s)
         => Items[Index++];
     public char Current
         => Items[Index];
+    public void Reset() 
+        => Index = 0;
 }
