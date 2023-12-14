cfc1667c0e09b622783c96c7d2e6f62d3f77a329
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-12-14 00:12:06 -0800

    remove and sort usings

--- aoc-solns-2023/day09/Sequence.cs
+++ aoc-solns-2023/day09/Sequence.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Numerics;
 namespace d9.aoc._23.day9;
 public class Sequence<T>
     where T : INumber<T>
