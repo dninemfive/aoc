4b1139ad22a216374fe9101f7c53a2039800aebd
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-12-14 16:29:23 -0800

    wait nvm that wasn't right

--- aoc-solns-2023/day11/Solution.cs
+++ aoc-solns-2023/day11/Solution.cs
@@ -1,16 +1,22 @@
-﻿namespace d9.aoc._23.day11;
+﻿using System.Runtime.CompilerServices;
+
+namespace d9.aoc._23.day11;
 public static class Solution
 {
     private static Grid<char> _grid;
+#pragma warning disable CS8618 // Initialized in .Solve() and other things are not public
+    private static HashSet<int> _emptyRows, _emptyColumns;
+#pragma warning restore CS8618
     [SolutionToProblem(11)]
     public static IEnumerable<object> Solve(string[] lines)
     {
         _grid = Grid<char>.From(lines);
+        (_emptyRows, _emptyColumns) = (EmptyRowIndices.ToHashSet(), EmptyColumnIndices.ToHashSet());
         yield return _grid.GalaxyLocations().ToList().UniquePairs().Select(GalaxyDistance).Sum();
     }
-    public static IEnumerable<Point<int>> GalaxyLocations(this Grid<char> grid)
+    private static IEnumerable<Point<int>> GalaxyLocations(this Grid<char> grid)
         => grid.AllPoints.Where(p => grid[p] == '#');
-    public static IEnumerable<(Point<int> a, Point<int> b)> UniquePairs(this List<Point<int>> Points)
+    private static IEnumerable<(Point<int> a, Point<int> b)> UniquePairs(this List<Point<int>> Points)
     {
         HashSet<(Point<int> a, Point<int> b)> seenPairs = new();
         bool hasBeenSeen(Point<int> a, Point<int> b) => seenPairs.Contains((a, b));
@@ -27,15 +33,18 @@ public static class Solution
             }
         }
     }
-    public static int EmptyRowsBetween(int aY, int bY)
-        => _grid.CountRowsWhereAll((t, _, y) => t == '.' && y.IsBetween(aY, bY));
-    public static int EmptyColumnsBetween(int aX, int bX)
-        => _grid.CountColumnsWhereAll((t, x, _) => t == '.' && x.IsBetween(aX, bX));
-    public static int GalaxyDistance(this (Point<int> a, Point<int> b) pair, int expansionFactor = 2)
+    private static IEnumerable<int> EmptyRowIndices
+        => _grid.Rows.Where(r => r.row.All(x => x == '.')).Select(r => r.index);
+    private static IEnumerable<int> EmptyColumnIndices
+        => _grid.Columns.Where(c => c.column.All(x => x == '.')).Select(c => c.index);
+    private static long GalaxyDistance(this (Point<int> a, Point<int> b) pair, int expansionFactor = 2)
     {
-        (Point<int> a, Point<int> b) = pair;
-        int emptySpacesBetweenPoints = EmptyRowsBetween(a.Y, b.Y) + EmptyColumnsBetween(a.X, b.X);
-        // -1 to avoid double-counting
-        return a.ManhattanDistanceFrom(b) + emptySpacesBetweenPoints * (expansionFactor - 1);
+        ((int x1, int y1), (int x2, int y2)) = pair;
+        int xStep = Math.Sign(x2 - x1), yStep = Math.Sign(y2 - y1);
+        long distance = 0;
+        for (int x = x1; x != x2; x += xStep)
+            for (int y = y1; y != y2; y += yStep)
+                distance += _emptyColumns.Contains(x) || _emptyRows.Contains(y) ? expansionFactor : 1;
+        return distance;
     }
 }
