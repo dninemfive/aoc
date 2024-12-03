e2f7a16034c655e7226b1d81d64980e3b7545a93
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-12-02 22:14:21 -0700

    successful solution to 2024-03.1

---
+++ src/2024/solns/day03/MulInstruction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace d9.aoc._24.day03;
+internal partial class MulInstruction(int left, int right)
+{
+    public static Regex Regex = MulRegex();
+    public int Value => left * right;
+    public static MulInstruction FromMatch(string match)
+    {
+        if (!Regex.IsMatch(match))
+            throw new Exception($"{match} does not match the regex for {typeof(MulInstruction).Name!}");
+        List<string> split = match.SplitAndTrim("(", ",", ")");
+        return new(int.Parse(split[1]), int.Parse(split[2]));
+    }
+
+    [GeneratedRegex(@"mul\(\d+,\d+\)")]
+    private static partial Regex MulRegex();
+}
---
+++ src/2024/solns/day03/Solution.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace d9.aoc._24.day03;
+[SolutionToProblem(3)]
+internal class Solution : AocSolution
+{
+    public override IEnumerable<AocPartialResult> Solve(params string[] lines)
+    {
+        yield return lines.SelectMany(x => MulInstruction.Regex.Matches(x))
+                          .Select(x => MulInstruction.FromMatch(x.Value).Value)
+                          .Sum();
+    }
+}
