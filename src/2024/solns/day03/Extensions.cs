28e644417e4bfd7c6630bcc33b6b07e3279146ae
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-12-02 22:41:32 -0700

    where tf are my fucking union types

---
+++ src/2024/solns/day03/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace d9.aoc._24.day03;
+internal static partial class Extensions
+{
+    private static readonly Regex _mulRegex = GenerateMulRegex();
+    public static Regex MulRegex => _mulRegex;
+    public static object Evaluate(this string match)
+    {
+        if (match.TryEvaluateDoOrDont() is bool b)
+            return b;
+        return match.TryEvaluateMul() ?? throw new ArgumentException($"{match} is not a valid instruction!", nameof(match));
+    }
+    public static bool? TryEvaluateDoOrDont(this string match)
+    {
+        if (match is not "do()" or "dont()")
+            return null;
+        return match == "do()";
+    }
+    public static int? TryEvaluateMul(this string match)
+    {
+        if (!MulRegex.IsMatch(match))
+            return null;
+        List<string> split = match.SplitAndTrim("(", ",", ")");
+        return int.Parse(split[1]) * int.Parse(split[2]);
+    }
+    [GeneratedRegex(@"mul\(\d+,\d+\)")]
+    private static partial Regex GenerateMulRegex();
+}
--- src/2024/solns/day03/Solution.cs
+++ src/2024/solns/day03/Solution.cs
@@ -2,12 +2,19 @@
 
 namespace d9.aoc._24.day03;
 [SolutionToProblem(3)]
-internal class Solution : AocSolution
+internal partial class Solution : AocSolution
 {
     public override IEnumerable<AocPartialResult> Solve(params string[] lines)
     {
-        yield return lines.SelectMany(x => MulInstruction.Regex.Matches(x))
-                          .Select(x => MulInstruction.FromMatch(x.Value).Value)
-                          .Sum();
+        IEnumerable<object> matches = lines.SelectMany<object>(x => x.Evaluate());
+        yield return "preinit";
+        yield return matches.SelectMany(x => MulInstruction.Regex.Matches(x))
+                            .Select(x => MulInstruction.FromMatch(x.Value).Value)
+                            .Sum();
+        IEnumerable<Match> matches = lines.SelectMany(x => AnyInstruction().Matches(x));
+        Console.WriteLine(matches.ListNotation());
     }
+    // til about the non-capturing modifier: https://stackoverflow.com/a/632248
+    [GeneratedRegex(@"(?:mul\(\d+,\d+\)|do\(\)|dont\(\))")]
+    private static partial Regex AnyInstruction();
 }
