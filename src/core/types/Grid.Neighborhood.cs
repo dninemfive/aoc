08f38a1b911af14406a0807f287f8f4e09541704
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-12-04 11:29:27 -0700

    fix index out of bounds in Subgrid

--- src/core/types/Grid.Neighborhood.cs
+++ src/core/types/Grid.Neighborhood.cs
@@ -10,7 +10,7 @@ public readonly partial struct Grid<T>
         (int bx, int by) = ClampInBounds(b);
         int x1 = Math.Min(ax, bx), x2 = Math.Max(ax, bx),
             y1 = Math.Min(ay, by), y2 = Math.Max(ay, by);
-        T[,] result = new T[x2 - x1, y2 - y1];
+        T[,] result = new T[x2 - x1 + 1, y2 - y1 + 1];
         for (int x = x1; x <= x2; x++)
             for (int y = y1; y <= y2; y++)
                 result[x - x1, y - y1] = this[x, y];
