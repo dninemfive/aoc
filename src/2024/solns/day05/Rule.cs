40dc07442d66a9c40a4319e77437d444f6568829
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-12-05 22:30:14 -0700

    some additional work on 2024-05

---
+++ src/2024/solns/day05/Rule.cs
@@ -0,0 +1,23 @@
+﻿namespace d9.aoc._24.day05;
+public class Rule(int first, int second)
+{
+    public int First => first;
+    public int Second => second;
+    public Rule(IEnumerable<int> ints) : this(ints.First(), ints.Second()) { }
+    public Rule(string line) : this(line.ToMany<int>("|")) { }
+    public override string ToString()
+        => $"{First}|{Second}";
+    public bool? ComesBefore(Rule other)
+    {
+        if (First == other.Second)
+            return false;
+        if (Second == other.First)
+            return true;
+        return null;
+    }
+    public void Deconstruct(out int first, out int second)
+    {
+        first = First;
+        second = Second;
+    }
+}
--- src/2024/solns/day05/Solution.cs
+++ src/2024/solns/day05/Solution.cs
@@ -5,36 +5,26 @@ internal class Solution : AocSolution
 {
     public override IEnumerable<AocPartialResult> Solve(params string[] lines)
     {
-        List<Order> orders = new();
-        foreach(string line in lines)
+        List<Rule> allRules = new();
+        List<IEnumerable<int>> allUpdates = new();
+        bool rule = true;
+        foreach (string line in lines)
         {
             if (line.IsNullOrEmpty())
-                break;
-            orders.Add(new Order(line));
+            {
+                rule = false;
+                continue;
+            }
+            if(rule)
+            {
+                allRules.Add(new Rule(line));
+            }
+            else
+            {
+                allUpdates.Add(line.ToMany<int>(","));
+            }
         }
-        foreach(Order order in orders.Order())
-            Console.WriteLine(order);
         yield return "preinit";
+        yield return allUpdates.Count(u => !allRules.Any(r => u.Violates(r)));
     }
-}
-public class Order(int first, int second)
-    : IComparable<Order>
-{
-    public int First => first;
-    public int Second => second;
-    public Order(IEnumerable<int> ints) : this(ints.First(), ints.Second()) { }
-    public Order(string line) : this(line.ToMany<int>("|")) { }
-    public int CompareTo(Order? other)
-    {
-        if(other is Order order)
-        {
-            int cmp = First.CompareTo(other.First);
-            if (cmp != 0)
-                return cmp;
-            return Second.CompareTo(other.First);
-        }
-        return -1;
-    }
-    public override string ToString()
-        => $"{First}|{Second}";
 }
