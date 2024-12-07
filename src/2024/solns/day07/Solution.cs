42dbbe857d4b87733602ea1e70cc53da3739f6c3
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-12-07 10:16:51 -0700

    non-working 2024-07.1 solution

--- src/2024/solns/day07/Solution.cs
+++ src/2024/solns/day07/Solution.cs
@@ -5,44 +5,61 @@ namespace d9.aoc._24.day07;
 [SampleResults(3749)]
 internal class Solution : AocSolution
 {
+    public static T Add<T>(T l, T r)
+        where T : INumber<T>
+        => l + r;
+    public static T Multiply<T>(T l, T r)
+        where T : INumber<T>
+        => l + r;
     public override IEnumerable<AocPartialResult> Solve(params string[] lines)
     {
-        throw new NotImplementedException();
+        List<Operation<int>> operations = [Add, Multiply];
+        yield return lines.Parse()
+                          .Where(x => x.numbers.PossibleSolutions(operations)
+                                               .Any(y => y == x.expected))
+                          .Select(x => x.expected)
+                          .Sum();
     }
-    public IEnumerable<(IEnumerable<int> numbers, int expected)> Parse(string[] lines)
+}
+public delegate T Operation<T>(T l, T r)
+    where T : INumber<T>;
+internal static class Extensions
+{
+    internal static IEnumerable<(IEnumerable<int> numbers, int expected)> Parse(this string[] lines)
     {
-        foreach(string line in lines)
+        foreach (string line in lines)
         {
             string[] split = line.Split(": ");
             yield return (split[1].ToMany<int>(), int.Parse(split[0]));
         }
     }
-    public delegate T Operation<T>(T l, T r)
-        where T : INumber<T>;
-    public T Add<T>(T l, T r)
-        where T : INumber<T>
-        => l + r;
-    public T Multiply<T>(T l, T r)
-        where T : INumber<T>
-        => l + r;
-    public IEnumerable<T> PossibleCombinations<T>(IEnumerable<T> operands)
+    internal static IEnumerable<T> PossibleSolutions<T>(this IEnumerable<T> operands, IEnumerable<Operation<T>> operations)
         where T : INumber<T>
     {
-        List<Operation<T>> operations = [Add, Multiply];
-        foreach(IEnumerable<Operation<T>> combination in AllCombinationsOf(operations, operands.Count() - 1))
+        foreach (IEnumerable<Operation<T>> combination in AllCombinationsOf(operations, operands.Count() - 1))
         {
             T result = operands.First();
-            for(int i = 0; i < combination.Count(); i++)
+            for (int i = 0; i < combination.Count(); i++)
             {
                 result = combination.ElementAt(i)(result, operands.ElementAt(i + 1));
             }
             yield return result;
         }
     }
-    public IEnumerable<IEnumerable<T>> AllCombinationsOf<T>(IEnumerable<T> options, int count)
+    internal static IEnumerable<IEnumerable<T>> AllCombinationsOf<T>(this IEnumerable<T> options, int count)
     {
+        ArgumentOutOfRangeException.ThrowIfLessThan(count, 1, nameof(count));
         if (count == 1)
+        {
             yield return options;
-        throw new NotImplementedException();
+            yield break;
+        }
+        foreach (T option in options)
+        {
+            foreach (IEnumerable<T> remainingOptions in AllCombinationsOf(options, count - 1))
+            {
+                yield return [option, .. remainingOptions];
+            }
+        }
     }
 }
