9bcef77c39231dcea583eaa67ad29fbcdde1a910
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-12-07 11:05:05 -0700

    restructure all of 2024-07.1 to improve clarity and output, still not working tho

---
+++ src/2024/solns/day07/Extensions.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+using System.Numerics;
+
+namespace d9.aoc._24.day07; 
+internal static class Extensions
+{
+    internal static IEnumerable<(IEnumerable<T> numbers, T expected)> Parse<T>(this string[] lines, IFormatProvider? provider = null)
+        where T : INumber<T>, IParsable<T>
+    {
+        provider ??= CultureInfo.InvariantCulture;
+        foreach (string line in lines)
+        {
+            string[] split = line.Split(": ");
+            yield return (split[1].ToMany<T>(), T.Parse(split[0], provider));
+        }
+    }
+    internal static IEnumerable<Expression<T>> PossibleExpressions<T>(this IEnumerable<T> operands, IEnumerable<Operator<T>> operatorOptions)
+        where T : INumber<T>
+    {
+        foreach (IEnumerable<Operator<T>> combination in AllCombinationsOf(operatorOptions, operands.Count() - 1))
+        {
+            yield return new(operands, combination);
+        }
+    }
+    internal static IEnumerable<IEnumerable<T>> AllCombinationsOf<T>(this IEnumerable<T> options, int count)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(count, 1, nameof(count));
+        if (count == 1)
+        {
+            foreach(T option in options)
+            {
+                yield return [option];
+            }
+            yield break;
+        }
+        foreach (T option in options)
+        {
+            foreach (IEnumerable<T> remainingOptions in AllCombinationsOf(options, count - 1))
+            {
+                yield return [option, .. remainingOptions];
+            }
+        }
+    }
+}
--- src/2024/solns/day07/Solution.cs
+++ src/2024/solns/day07/Solution.cs
@@ -1,65 +1,33 @@
 ﻿using System.Numerics;
+using Expression    = d9.aoc._24.day07.Expression<long>;
+using Operator      = d9.aoc._24.day07.Operator<long>;
+using Operators     = d9.aoc._24.day07.Operators<long>;
 
 namespace d9.aoc._24.day07;
 [SolutionToProblem(7)]
-[SampleResults(3749)]
+[SampleResults(3749L)]
 internal class Solution : AocSolution
 {
-    public static T Add<T>(T l, T r)
-        where T : INumber<T>
-        => l + r;
-    public static T Multiply<T>(T l, T r)
-        where T : INumber<T>
-        => l + r;
     public override IEnumerable<AocPartialResult> Solve(params string[] lines)
     {
-        List<Operation<int>> operations = [Add, Multiply];
-        yield return lines.Parse()
-                          .Where(x => x.numbers.PossibleSolutions(operations)
+        List<Operator> operations = [Operators.Add, Operators.Multiply];
+        List<(IEnumerable<Expression> expr, long expected)> equations
+            = lines.Parse<long>()
+                   .Select(x => (x.numbers.PossibleExpressions(operations), x.expected))
+                   .ToList();
+        foreach((IEnumerable<Expression> expressions, long expectedResult) in equations)
+        {
+            Console.WriteLine("=======");
+            Console.WriteLine($"Expected result: {expectedResult}");
+            foreach(Expression expression in expressions)
+            {
+                Console.WriteLine($"{expression} = {expression.Evaluate()}");
+            }
+        }
+        yield return lines.Parse<long>()
+                          .Where(x => x.numbers.PossibleExpressions(operations)
                                                .Any(y => y == x.expected))
                           .Select(x => x.expected)
                           .Sum();
-    }
-}
-public delegate T Operation<T>(T l, T r)
-    where T : INumber<T>;
-internal static class Extensions
-{
-    internal static IEnumerable<(IEnumerable<int> numbers, int expected)> Parse(this string[] lines)
-    {
-        foreach (string line in lines)
-        {
-            string[] split = line.Split(": ");
-            yield return (split[1].ToMany<int>(), int.Parse(split[0]));
-        }
-    }
-    internal static IEnumerable<T> PossibleSolutions<T>(this IEnumerable<T> operands, IEnumerable<Operation<T>> operations)
-        where T : INumber<T>
-    {
-        foreach (IEnumerable<Operation<T>> combination in AllCombinationsOf(operations, operands.Count() - 1))
-        {
-            T result = operands.First();
-            for (int i = 0; i < combination.Count(); i++)
-            {
-                result = combination.ElementAt(i)(result, operands.ElementAt(i + 1));
-            }
-            yield return result;
-        }
-    }
-    internal static IEnumerable<IEnumerable<T>> AllCombinationsOf<T>(this IEnumerable<T> options, int count)
-    {
-        ArgumentOutOfRangeException.ThrowIfLessThan(count, 1, nameof(count));
-        if (count == 1)
-        {
-            yield return options;
-            yield break;
-        }
-        foreach (T option in options)
-        {
-            foreach (IEnumerable<T> remainingOptions in AllCombinationsOf(options, count - 1))
-            {
-                yield return [option, .. remainingOptions];
-            }
-        }
     }
 }
