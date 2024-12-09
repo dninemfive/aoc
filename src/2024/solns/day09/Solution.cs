4307f360b67de7ab1caac8b65c8efc5252bcb192
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-12-08 22:45:58 -0700

    initial pass at 2024.09 logic

---
+++ src/2024/solns/day09/Solution.cs
@@ -0,0 +1,30 @@
+﻿
+namespace d9.aoc._24.day09;
+[SolutionToProblem(9)]
+[SampleResults(1928)]
+internal class Solution : AocSolution
+{
+    public override IEnumerable<AocPartialResult> Solve(params string[] lines)
+    {
+        Filesystem filesystem = new(lines.First());
+        Console.WriteLine(filesystem);
+        yield return "preinit";
+        yield return 1928;
+    }
+}
+internal class Filesystem
+{
+    internal readonly List<int?> Files = new();
+    public Filesystem(string line)
+    {
+        for(int i = 0; i < line.Length; i++)
+        {
+            int blockSize = int.Parse($"{line[i]}");
+            int? blockContents = i % 2 == 0 ? i / 2 : null;
+            for (int j = 0; j < blockSize; j++)
+                Files.Add(blockContents);
+        }
+    }
+    public override string ToString()
+        => Files.Select(x => x.PrintNull(".")).Join();
+}
