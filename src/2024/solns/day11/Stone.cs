b27f101885ffed20ab7c44cd48f074d6b14f862c
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-12-11 14:59:03 -0700

    start on something else ig

---
+++ src/2024/solns/day11/Stone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d9.aoc._24.day11;
+internal record Stone<T>()
+    where T : INumber<T>
+{
+    public async Task<IEnumerable<Stone<T>>> Evaluate()
+    {
+
+    }
+}
