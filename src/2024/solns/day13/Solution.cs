7a602069767683a68824032aec975f54f568f507
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-12-16 23:49:37 -0700

    some optimization ig

--- src/2024/solns/day13/Solution.cs
+++ src/2024/solns/day13/Solution.cs
@@ -1,32 +1,39 @@
 ﻿
+using System.Numerics;
 using System.Text.RegularExpressions;
 
 namespace d9.aoc._24.day13;
 [SolutionToProblem(13)]
-[SampleResults(480)]
+[SampleResults(480L)]
+[FinalResults(40069L)]
 internal class Solution : AocSolution
 {
     public override IEnumerable<AocPartialResult> Solve(params string[] lines)
     {
-        IEnumerable<ClawMachine> clawMachines = lines.Chunk(4)
+        IEnumerable<ClawMachine<long>> clawMachines1 = lines.Chunk(4)
                                                    //.Select(x => x.Where(y => !y.IsNullOrEmpty()))
-                                                     .Select(ClawMachine.FromLines);
-        yield return clawMachines.Select(x => x.MinComboCost()).Where(x => x is not null).Sum()!;
+                                                     .Select(ClawMachine<long>.FromLines);
+        yield return clawMachines1.Select(x => x.MinComboCost()).Sum();
+        yield break;
+        Point<long> offset = (10000000000000L, 10000000000000L);
+        IEnumerable<ClawMachine<long>> clawMachines2 = clawMachines1.Select(x => new ClawMachine<long>(x.ButtonA, x.ButtonB, x.Prize + offset));
+        yield return clawMachines2.Select(x => x.MinComboCost()).Sum();
     }
 }
-internal partial record Button(int XOffset, int YOffset, string Name)
+internal partial record Button<T>(T XOffset, T YOffset, string Name)
+    where T : INumber<T>
 {
     public static readonly Regex ButtonRegex = GenerateButtonRegex();
-    public int Value => Name switch
+    public T Value => Name switch
     {
-        "A" => 3,
-        "B" => 1,
+        "A" => T.CreateChecked(3),
+        "B" => T.One,
         _ => throw new Exception($"{Name} is not a valid button name!")
     };
-    public Point<int> Offset => (XOffset, YOffset);
-    public static implicit operator Button((int a, int b, string c) tuple)
+    public Point<T> Offset => (XOffset, YOffset);
+    public static implicit operator Button<T>((T a, T b, string c) tuple)
         => new(tuple.a, tuple.b, tuple.c);
-    public static Button? FromLine(string line)
+    public static Button<T>? FromLine(string line)
     {
         MatchCollection matches = ButtonRegex.Matches(line);
         if (!matches.Any())
@@ -37,15 +44,16 @@ internal partial record Button(int XOffset, int YOffset, string Name)
                                      .Skip(1)
                                      .Select(x => x.Value)
                                      .ToList();
-        return new(int.Parse(groups[1]), int.Parse(groups[2]), groups[0]);
+        return new(T.Parse(groups[1], null), T.Parse(groups[2], null), groups[0]);
     }
     [GeneratedRegex(@"Button (.): X\+(\d+), Y\+(\d+)")]
     private static partial Regex GenerateButtonRegex();
 }
-internal partial record Prize(int X, int Y)
+internal partial record Prize<T>(T X, T Y)
+    where T : INumber<T>
 {
     public static readonly Regex PrizeRegex = GeneratePrizeRegex();
-    public static Prize? FromLine(string line)
+    public static Prize<T>? FromLine(string line)
     {
         MatchCollection matches = PrizeRegex.Matches(line);
         if (!matches.Any())
@@ -56,44 +64,55 @@ internal partial record Prize(int X, int Y)
                                      .Skip(1)
                                      .Select(x => x.Value)
                                      .ToList();
-        return new(int.Parse(groups.First()), int.Parse(groups.Second()));
+        return new(T.Parse(groups.First(), null), T.Parse(groups.Second(), null));
     }
-    public static implicit operator Point<int>(Prize prize)
+    public static implicit operator Point<T>(Prize<T> prize)
         => (prize.X, prize.Y);
     [GeneratedRegex(@"Prize: X=(\d+), Y=(\d+)")]
     private static partial Regex GeneratePrizeRegex();
 }
-internal partial record ClawMachine(Button ButtonA, Button ButtonB, Point<int> Prize)
+internal partial record ClawMachine<T>(Button<T> ButtonA, Button<T> ButtonB, Point<T> Prize)
+    where T : INumber<T>
 {
-    public static ClawMachine FromLines(IEnumerable<string> lines)
+    public static ClawMachine<T> FromLines(IEnumerable<string> lines)
     {
-        Dictionary<string, Button?> buttons = lines.Select(Button.FromLine)
+        Dictionary<string, Button<T>?> buttons = lines.Select(Button<T>.FromLine)
                                                    .Where(x => x is not null)
                                                    .ToDictionary(x => x!.Name);
-        Prize prize = lines.Select(day13.Prize.FromLine)
+        Prize<T> prize = lines.Select(Prize<T>.FromLine)
                                               .Where(x => x is not null)
                                               .First()!;
         return new(buttons["A"]!, buttons["B"]!, prize);
     }
-    public IEnumerable<(int a, int b)> PotentialButtonPressCombos()
+    public IEnumerable<(T a, T b)> PotentialButtonPressCombos()
     {
         // find solutions to aA + bB = P
-        int max = 100;
-        for(int a = 0; a < max; a++)
-            for(int b = 0; b < max; b++)
+        IEnumerable<T> coefficients = [
+            Prize.X / ButtonA.XOffset,
+            Prize.Y / ButtonA.YOffset,
+            Prize.X / ButtonB.XOffset,
+            Prize.Y / ButtonB.YOffset
+        ];
+        // Console.WriteLine($"{coefficients.ListNotation()} {coefficients.Max().PrintNull()}");
+        T min = T.Zero,
+          aMax = coefficients.Take(2).Min()!,
+          bMax = coefficients.Skip(2).Max()!;
+        for(T a = min; a <= aMax; a++)
+            for(T b = min; b <= bMax; b++)
             {
                 //Console.WriteLine($"{a,3}\t{b,3}\t{ButtonA.Offset}\t{ButtonA.Offset * a}\t{ButtonB.Offset}\t{ButtonB.Offset * b}\t{ButtonA.Offset * a + ButtonB.Offset * b}\t{Prize}");
                 if (ButtonA.Offset * a + ButtonB.Offset * b == Prize)
                     yield return (a, b);
             }
     }
-    public int ComboCost((int a, int b) combo)
+    public T ComboCost((T a, T b) combo)
         => ButtonA.Value * combo.a + ButtonB.Value * combo.b;
-    public (int a, int b)? MinCostCombo()
+    public (T a, T b)? MinCostCombo()
     {
-        IEnumerable<(int a, int b)> potentialCombos = PotentialButtonPressCombos();
+        IEnumerable<(T a, T b)> potentialCombos = PotentialButtonPressCombos();
+        // Console.WriteLine(potentialCombos.ListNotation());
         return potentialCombos.Any() ? potentialCombos.MinBy(ComboCost) : null;
     }
-    public int? MinComboCost()
-        => MinCostCombo() is (int, int) t ? ComboCost(t) : null;
+    public T MinComboCost()
+        => MinCostCombo() is (T, T) t ? ComboCost(t) : T.Zero;
 }
