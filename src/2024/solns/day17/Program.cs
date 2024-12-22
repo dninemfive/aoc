c243920d9503b32c2eddbef96669b267e7dd1bca
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-12-21 23:50:59 -0700

    2024-17: full but untested solution

--- src/2024/solns/day17/Program.cs
+++ src/2024/solns/day17/Program.cs
@@ -9,12 +9,32 @@ internal class Program<T>(ProgramState<T> initialState, IEnumerable<T> instructi
 {
     public ProgramState<T> State = initialState;
     public IList<T> Instructions = instructions.ToList();
+    public (int opcode, T operand)? NextOperation
+    {
+        get
+        {
+            if (State.InstructionPointer + 1 < Instructions.Count)
+            {
+                return (int.CreateChecked(Instructions[State.InstructionPointer]),
+                                          Instructions[State.InstructionPointer + 1]);
+            }
+            else return null;
+        }
+    }
     public bool RunNextInstruction()
     {
-        if(State.InstructionPointer + 1 < Instructions.Count)
+        if (NextOperation is (int opcode, T operand))
         {
-            int opcode = int.CreateChecked(Instructions[State.InstructionPointer]);
+            State = Instructions<T>.Call(State, opcode, operand);
+            return true;
         }
         else return false;
     }
+    public void RunToCompletion()
+    {
+        while (RunNextInstruction())
+            ;
+    }
+    public string OutputString => State.Output.Select(x => x.ToString()!)
+                                              .JoinWithDelimiter(",");
 }
