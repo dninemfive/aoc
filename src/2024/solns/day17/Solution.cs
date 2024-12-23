58880c610ee2f86cf43be3743b0dbb34bec9db46
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-12-22 23:06:01 -0700

    2024-17: solution for part 1

--- src/2024/solns/day17/Solution.cs
+++ src/2024/solns/day17/Solution.cs
@@ -1,13 +1,15 @@
 ﻿
 namespace d9.aoc._24.day17;
 [SolutionToProblem(17)]
-[SampleResults("4,6,3,5,6,3,5,2,1,0")]
+[SampleResults("4,6,3,5,6,3,5,2,1,0", 117440)]
+[FinalResults("2,1,3,0,5,2,3,7,1")]
 internal class Solution
     : AocSolution
 {
     public override IEnumerable<AocPartialResult> Solve(params string[] lines)
     {
         Program<int> program = Program<int>.FromLines(lines);
+        ProgramState<int> initialState = program.State;
         program.RunToCompletion();
         yield return new(program.OutputString);
     }
